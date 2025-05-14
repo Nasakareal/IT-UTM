edee94169db5412dad815a23c9ec254446be0010
Mario Bautista <naasaka@outlook.es>
2025-05-14 00:45:10 -0600

    antes si la habia cagado

--- resources/views/modulos/show.blade.php
+++ resources/views/modulos/show.blade.php
@@ -124,7 +124,7 @@
                 <!-- Botón solo para módulo 5 y subsección Gestión Académica -->
                 @if(strtoupper($subsec->nombre) === 'GESTIÓN ACADÉMICA' && $modulo->id == 5)
                     <div class="text-end me-4 mb-3">
-                        <a href="{{ route('modulo7.gestion') }}" class="btn btn-primary">
+                        <a href="{{ route('modulo5.gestion') }}" class="btn btn-primary">
                             <i class="fas fa-file-alt"></i> Ver Documentos por Unidad
                         </a>
                     </div>
